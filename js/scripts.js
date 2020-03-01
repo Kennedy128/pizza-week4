a97ce973df692dbbab8ea896bea169085fd2d3dc
Santa claus <santa@northpole.com>
2020-03-01 22:32:33 +0300

    addition of tables

--- js/scripts.js
+++ js/scripts.js
@@ -129,10 +129,10 @@ $(document).ready(function(){
     let checkoutTotal =0;
     checkoutTotal = checkoutTotal + total;
 
-    $("#pizzaname").html($(".name option:selected").val());
-    $("#pizzasize").html( $("#size option:selected").val());
-    $("#pizzacrust").html($("#crust option:selected").val());
-    $("#pizzatopping").html(ptopping.join(", "));
+    $("#pizza-names").html($(".name option:selected").val());
+    $("#pizza-sizes").html( $("#size option:selected").val());
+    $("#pizza-crusts").html($("#crust option:selected").val());
+    $("#pizza-toppings").html(ptopping.join(", "));
     $("#totals").html(total);
     
 // Add pizza button
